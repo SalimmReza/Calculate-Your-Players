d4ecc4788a989b387e825ece11db9006575367e7
SalimmReza <salimnuaa@gmail.com>
2022-08-22 01:39:19 +0600

    player expense added

--- js/index.js
+++ js/index.js
@@ -1,33 +1,69 @@
 cart = [];
-document.getElementById('players-card').addEventListener('click', function (event) {
-    const player = event.target.parentNode.childNodes[1].innerText;
-    const playerName = {
-        Name: player
-    }
-    cart.push(playerName);
+
+//player details top card
+function displayProduct() {
     const playersDetails = document.getElementById("players-name");
 
 
-    if (cart.length > 5) {
-        alert('Only 5 players allowed')
-
-    }
-    else {
-        playersDetails.innerText = '';
+    if (cart.length <= 5) {
+        playersDetails.textContent = '';
         for (let i = 0; i < cart.length; i++) {
-
             const tr = document.createElement("tr");
             tr.innerHTML = `
-            <th>${i + 1}</th>
-            <td>${cart[i].Name}</td>
+             <th>${i + 1}</th>
+             <td>${cart[i].pdName}</td>
             
-            `;
+             `;
             playersDetails.appendChild(tr);
 
 
         }
+
     }
-});
+    else {
+        alert('jknfjr');
+        return;
+    }
+
+    document.getElementById('calculate-btn').addEventListener('click', function () {
+        const perPlayerField = document.getElementById('per-player-field');
+        const playerFieldString = perPlayerField.value;
+        const playerField = parseFloat(playerFieldString);
+
+
+        //calcualtion
+        const totalPlayerAmount = playerField * cart.length;
+        console.log(totalPlayerAmount);
+
+    });
+
+
+};
+
+
+// click button card
+function addToCart(element) {
+    const player = element.parentNode.children[0].innerText;
+    // const button = element.parentNode.children[1];
+    // if (button === element) {
+    //     button.style.display = 'none';
+
+    // } 
+
+    const pd = {
+        pdName: player
+    }
+
+    cart.push(pd);
+
+    displayProduct();
+
+
+};
+
+
+
+
 
 
 
